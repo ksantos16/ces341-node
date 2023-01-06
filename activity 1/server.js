7e634f09faf60e994659ee8dad87848e7b278bc6
ksantos16 <u121364866@byui.edu>
2023-01-06 00:59:03 -0500

    .env

--- activity 1/server.js
+++ activity 1/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+mongoose.connect(`mongodb://<dbuser>:<dbpassword>@ds241658.mlab.com:41658/test_db`,(err)=>{
+if(err) throw err;
+console.log("DB Connected Successfully");
+})
+
 const port = process.env.PORT || 3001;
 
 app.use('/', require('./routes'))
