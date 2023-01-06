82de05f5123039e3b053f739a9125470cefa2823
ksantos16 <u121364866@byui.edu>
2023-01-06 01:37:48 -0500

    json package

--- activity 1/server.js
+++ activity 1/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 const app = express();
 
-mongoose.connect(`mongodb://<dbuser>:<dbpassword>@ds241658.mlab.com:41658/test_db`,(err)=>{
-if(err) throw err;
-console.log("DB Connected Successfully");
-})
+// mongoose.connect(`mongodb://<dbuser>:<dbpassword>@ds241658.mlab.com:41658/test_db`,(err)=>{
+// if(err) throw err;
+// console.log("DB Connected Successfully");
+// })
 
 const port = process.env.PORT || 3001;
 
