17da5f3a5dc24e0c5b7304fa5cea374d1048df24
ksantos16 <u121364866@byui.edu>
2023-01-25 23:54:16 -0500

    swagger sucess mess

--- src/server.js
+++ src/server.js
@@ -6,7 +6,11 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
