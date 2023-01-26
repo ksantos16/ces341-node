13754d36150aca4736ca9659aa4a523088484593
ksantos16 <u121364866@byui.edu>
2023-01-25 23:54:04 -0500

    swagger sucess mess

---
+++ src/models/contacts.js
@@ -0,0 +1,18 @@
+module.exports = (mongoose) => {
+    const Contact = mongoose.model(
+      'contacts',
+      mongoose.Schema(
+        {
+     
+          firstName: String,
+          lastName: String,
+          email: String,
+          favoriteColor: String,
+          birthday: String,
+        },
+        { timestamps: true }
+      )
+    );
+  
+    return Contact;
+  };
