77a7ec1568188012da2875d074435d88f5fc5d44
ksantos16 <u121364866@byui.edu>
2023-01-26 21:01:48 -0500

    swaggerdoc path corrected

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('../swagger.json'));
+router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'))
 
 module.exports = router;
