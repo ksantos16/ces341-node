afe55ff0ab240e75fe46c7865ad30ff136f442e5
ksantos16 <u121364866@byui.edu>
2023-01-27 19:03:23 -0500

    update to host

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Contacts API',
   },
-  host: 'https://cse341-activity1.onrender.com',
+  host: 'cse341-activity1.onrender.com',
   schemes: ['http'],
 };
 
